7f18c02ef46c398ffbb0f49a12a3531a1f9c58ab
Growing-mold <nkrtsn@yandex.ru>
2023-05-15 22:56:02 +0300

    функция деления на ноль добавлена

--- calculate/calculate/Program.cs
+++ calculate/calculate/Program.cs
@@ -35,7 +35,14 @@ namespace calculate
                     result = num1 * num2;
                     break;
                 case '/':
-                    result = num1 / num2;
+                    if (num2 !=0)
+                    {
+                        result = num1 / num2;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Cannot divide by zero");
+                    }
                     break;
                 default:
                     Console.WriteLine("Invalid operator");
